2097cd594b6de81e883d535d1d48a2537320dc9d
Nils897 <fleschhut.nils@gmail.com>
2025-02-07 16:01:15 +0100

    Copy Files from other Repo

--- fibonacci.h
+++
@@ -1,8 +0,0 @@
-#ifndef FIBONACCI_H
-#define FIBONACCI_H
-
-unsigned int fibonacci( unsigned int count );
-
-void fibonacci_test( void );
-
-#endif
--- var_test_2.c
+++
@@ -1,8 +0,0 @@
-//
-// Created by infohoschie on 09.12.24.
-//
-
-#include "var_test_2.h"
-
-static short varStatA;
-static short varStatB = 9;
